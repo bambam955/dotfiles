0a56f0c7bad31e344bff432e950a2bfa9c97b02e
Bennett Moore <bennett@bemoore.life>
2026-02-13 12:49:54 -0500

    feat: add MRS_SDK_QT_ROOT to work profile

--- profiles/work/bash/4-aliases.work.bash
+++ profiles/work/bash/4-aliases.work.bash
@@ -16,9 +16,12 @@ sscp() {
 
 # Other apps
 alias qtc='~/Qt/Tools/QtCreator/bin/qtcreator . > /dev/null 2>&1 &'
-[[ -d "${HOME}/mrs-sdk-qt/tools" ]] && add_to_path "${HOME}/mrs-sdk-qt/tools"
 
 # VPN management
 VPN_CONFIG_FILE="${HOME}/Documents/important/sslvpn-bennett.moore@mrs-electronics.com-client-config.ovpn"
 alias vpn='sudo openvpn3 session-start --config "${VPN_CONFIG_FILE}"'
 alias stopvpn='sudo openvpn3 session-manage --config "${VPN_CONFIG_FILE}" --disconnect'
+
+# MRS SDK Qt config
+export MRS_SDK_QT_ROOT="${HOME}/mrs-sdk-qt"
+[[ -d "${HMRS_SDK_QT_ROOT}/tools" ]] && add_to_path "${MRS_SDK_QT_ROOT}/tools"
